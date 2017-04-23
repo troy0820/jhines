b0c0043b714de1d08e7db046742934fbec89bc6e
Troy Connor <troy0820@gmail.com>
2017-04-23 13:01:14 -0400

    [contact]Add contact page

---
+++ js/orbit.js
@@ -0,0 +1,12 @@
+$(window).load(function() {
+    $('#featured').orbit({
+        animation: 'fade',
+        bullets: false,
+        captionAnimation: 'fade',
+        directionalNav: false,
+        timer: true,
+        animationSpeed: 800,
+        advancedSpeed: 400,
+        fluid: true,
+    });
+});
